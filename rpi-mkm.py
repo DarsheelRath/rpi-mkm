7f884fe1dde20aa6a51e86135d3f938219203b0b
DarsheelRath <darsheel.rath@gmail.com>
2025-06-27 16:11:42 -0500

    Adding OpenCV support to rpi-mkm

--- rpi-mkm.py
+++ rpi-mkm.py
@@ -18,3 +18,23 @@ def mcpi():
     mc.postToChat("Start Building!")
     while True:
         pass
+def OpenCV_test():
+    print('Configure this file for a diffrent image, and download a picture with realistic faces with the name "opencv-facedetect.jpg, and change the letter x to your python version"')
+    import cv2
+
+    cv2data = '/usr/local/lib/pythonx/dist-packages/cv2/data'
+    facecascade = (cv2data +  'haarcascade_frontalface_default.xml')
+    img = cv2.data.imread('opencv-facedetect.jpg')
+    scale_factor = 1.4
+    min_neighbours = 5
+
+    faces = face_cascade.detectMultiScale(img, scale_factor, min_neighbours)
+    print(faces)
+
+    for (x,y,w,h) in faces:
+        img = cv2.rectangle(img, (x,y), (x+w,y+h), (255, 255, 255), 2)
+
+    cv2.imshow('image',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
