fdd863719825e24e33714557b77f17c09c07858d
DarsheelRath <darsheel.rath@gmail.com>
2025-06-27 16:19:40 -0500

    Adding Installations to setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
-
+from setuptools import setup, find_packages 
+setup( name="rpi-mkm",
+      version="1.0.0",
+      description="A brief description of your package",
+      author="Darsheel Rath",
+      author_email="darsheelrath@gmail.com",
+      packages=find_packages(),
+      install_requires=[ # List your dependencies here ],
+      classifiers=[ 'Development Status :: 3 - Alpha', 'License :: OSI Approved :: MIT License', 'Programming Language :: Python :: 3.11', ], )
